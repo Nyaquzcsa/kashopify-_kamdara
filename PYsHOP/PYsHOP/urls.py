77ac355ec7820b7aa136015529b7a6cdfef4a0ce
Nyaquzcsa <nyaquwaniqwa@gmail.com>
2023-09-13 17:36:56 +0200

    Secondary commit after creating superuser

--- PYsHOP/PYsHOP/urls.py
+++ PYsHOP/PYsHOP/urls.py
@@ -17,10 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-django.contrib import admin
+from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('produce' )
+    path('produce/', include('produce.urls'))
 ]
--- PYsHOP/produce/apps.py
+++ PYsHOP/produce/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class ProduceConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'PRODUCE'
+    name = 'produce'
